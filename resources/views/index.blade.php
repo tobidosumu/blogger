b106f5b38c7588426a5b71603ee7589ab677fb77
tobidosumu <dosunmumishael@gmail.com>
2023-02-27 15:35:20 +0100

    add: footer and footer top

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="pt-8 pb-12 border border-red-600">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-1">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
